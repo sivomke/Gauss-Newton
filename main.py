cf8bd5fa7b1f6a75e682940d65e329ea7c46e004
sivomke <ocean1641@gmail.com>
2020-02-10 16:39:44 +0200

    implemented Levenberg-Marquardt algo compaired it to built-in version in scipy

--- main.py
+++ main.py
@@ -1,34 +1,44 @@
 import numpy as np
+import time
+from scipy.optimize import least_squares
 
+start_time = time.time()
 
 # find coordinates of a point in N-dim space with Euclidean metric
 # given distances to m points with known coordinates
-
+# given distances are disturbed with Gaussian noise
 
 N = 2  # dimension
-m = 1000  # number of known points
-eps = pow(10, -6)
+m = 100  # number of known points
+eps = pow(10, -8)
+
 
 # square of Euclidean distance in N-dim space
-def dist(x, y):
+def distance(x, y) -> np.array:
     return np.dot(x-y, x-y)
 
 
 target = np.array([12, 18])
+print("target point: {}".format(target))
 
-points = 100*np.random.rand(m, N)  #given points
-dist_arr = np.array([dist(target, points[i]) for i in range(m)])  # given distances
+points = 100*np.random.rand(m, N)  # given points
+dist_arr = np.array([distance(target, points[i]) for i in range(m)])  # given distances
 
 # adding Gaussian noise to measurements with the following parameters:
 mu = 0  # mean
 sigma = 50
 dist_arr_disturbed = dist_arr + np.random.normal(mu, sigma, m)
-# dist_arr_disturbed = dist_arr + np.random.rand(m)
 
 
-# residual for i-th point; difference btw calculated and actual distance btw x and points[i]
+# residual for i-th point
 def r_i(i, x):
-    return dist(x, points[i]) - dist_arr_disturbed[i]
+    return distance(x, points[i]) - dist_arr_disturbed[i]
+
+
+# squared euclidean norm of error function
+# we want to find argmin of f(x)
+def f(x):
+    return np.sum(np.array([r_i(i, x)**2 for i in range(m)]))
 
 
 def dr_i_dx_j(x, i, j):
@@ -39,27 +49,43 @@ def jacobian(x):
     return np.array([[dr_i_dx_j(x, i, j) for j in range(N)] for i in range(m)])
 
 
-# x_cur = np.array([0, 0])
-x_cur = np.random.rand(2)
-print("initial guess: {}".format(x_cur))
-steps = 0
-
-# for i in range(10):
-while True:
-    J = jacobian(x_cur)
-    J_T = J.transpose()
-    inverse = np.linalg.inv(np.matmul(J_T, J))
-    r = np.array([r_i(i, x_cur) for i in range(m)])
-    delta = np.matmul(inverse, J_T).dot(r)
-    print(delta)
-    if abs(np.dot(delta, delta))<eps:
-        break
-    x_cur = x_cur - delta
-    steps += 1
-    print(x_cur)
+# function which computes the vector of residuals
+def g(x):
+    return np.array([r_i(i, x) for i in range(m)])
 
 
-print("Number of iterations: {}".format(steps))
+# custom implementation of Levenberg-Marquardt
+def lm():
+    x = np.random.rand(2)
+    print("initial guess: {}".format(x))
+    steps = 0  # counts number of iterations until the solution is found
+    damping_factor = .001 # regularization
+    eye = np.identity(N)
+    while True:
+        J = jacobian(x)
+        J_T = J.T
+        inverse = np.linalg.inv(J_T @ J + damping_factor*eye)
+        r = np.array([r_i(i, x) for i in range(m)])
+        delta = (inverse @ J_T).dot(r)
+        if f(x - delta) < f(x):
+            if abs(np.dot(delta, delta)) < eps:
+                break
+            x = x - delta
+            damping_factor *= 0.8
+            # steps += 1
+        else:
+            damping_factor *= 2
+        steps += 1
+        # print("# {} iteration".format(steps))
+        # print("damping factor: {:.6f}".format(damping_factor))
+        # print("computed point: {}".format(x))
+    return x
 
 
-
+res_1 = least_squares(g, np.random.rand(2))
+res_2 = lm()
+print(res_1.x)
+print(res_2)
+print(f(res_1.x))
+print(f(res_2))
+print(f(target))
